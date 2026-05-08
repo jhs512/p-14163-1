e55211184b5e54a5e1e3991784b2fbf4643a740e
jhs512 <jangka512@gmail.com>
2026-05-08 10:03:24 +0900

    012

--- src/main/java/com/mysite/sbb/Question.java
+++ src/main/java/com/mysite/sbb/Question.java
@@ -21,7 +21,6 @@ public class Question {
     @Column(columnDefinition = "TEXT")
     private String content;
 
-    // mappedBy 속성을 지정하지 않으면 중간테이블을 만든다.
-    @OneToMany(cascade = CascadeType.REMOVE)
+    @OneToMany(mappedBy = "question", cascade = CascadeType.REMOVE)
     private List<Answer> answers;
 }
