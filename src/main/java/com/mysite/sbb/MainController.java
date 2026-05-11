336ce7f9aa520fafda99f3a842012afd48eec92f
jhs512 <jangka512@gmail.com>
2026-05-11 10:36:05 +0900

    033

--- src/main/java/com/mysite/sbb/MainController.java
+++ src/main/java/com/mysite/sbb/MainController.java
@@ -2,19 +2,11 @@ package com.mysite.sbb;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class MainController {
     @GetMapping("/")
-    @ResponseBody
     public String index() {
-        return "SBB";
-    }
-
-    @GetMapping("/hello")
-    @ResponseBody
-    public String hello() {
-        return "안녕하세요 ^ ^";
+        return "redirect:/question/list";
     }
 }
